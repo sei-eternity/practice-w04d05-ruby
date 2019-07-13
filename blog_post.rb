f0b733677087cfd7c75d99a33b361f9b3900eab0
Eman-sh <amoooony@hotmail.com>
2019-07-13 14:26:48 +0300

    add comments to blog_post

--- blog_post.rb
+++ blog_post.rb
@@ -1,13 +1,17 @@
 ##
 # TODO: Add Comments to BlogPost Class
-
+#Creat a class that will contain posts infos = title, content and owner
 class BlogPost
+  # setup getters and setters 
   attr_reader :title, :content, :owner
 
+  # Initialize the class Properties 
   def initialize(title, content, owner = nil)
     @title, @content, @owner = title, content, owner
   end
 
+# this method prints one of two texts depending on whether the title and 
+# content are empty or not
   def edit(title, content)
     if title.empty? || content.empty?
       puts "Empty parameters are not allowed!"
