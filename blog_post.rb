23301ed3135351fbbb6b18f57e1c2691a643a489
Bedour47 <bedour.ir@gmail.com>
2019-07-13 21:03:04 +0300

    refactor the main.rb into app.rb + implement the "edit" method

--- blog_post.rb
+++ blog_post.rb
@@ -3,7 +3,8 @@
 
 class BlogPost
   # Setup our Getter and Setter methods for (Class Properties)
-  attr_reader :title, :content, :owner
+  attr_reader :owner
+  attr_accessor :title, :content
 
   def initialize(title, content, owner = nil)
     @title, @content, @owner = title, content, owner
@@ -12,13 +13,14 @@ class BlogPost
   ##
   # edit method takes two parameters (title, content)
   # first checks if any of the parameters are missing
-  # else it will allow the user to edit the post (for now, it only prints a message)
+  # else it will allow the user to edit the post
   #
   def edit(title, content)
     if title.empty? || content.empty?
       puts "Empty parameters are not allowed!"
     else
-      puts "Ready to Edit the Post"
+      self.title = title
+      self.content = content
     end
   end
 end
